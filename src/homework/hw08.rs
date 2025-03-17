9cd603b54b7e69256f39eef4ddfcc585d5841954
Zagiller <agalcun7@gmail.com>
2025-03-17 11:21:17 +0200

    Update hw08.rs

---
+++ src/homework/hw08.rs
@@ -0,0 +1,25 @@
+fn is_prime(n: u32) -> bool {
+    if n < 2 {
+        return false;
+    }
+    if n == 2 || n == 3 {
+        return true;
+    }
+    if n % 2 == 0 || n % 3 == 0 {
+        return false;
+    }
+    
+    let mut i = 5;
+    while i * i <= n {
+        if n % i == 0 || n % (i + 2) == 0 {
+            return false;
+        }
+        i += 6;
+    }
+    true
+}
+
+fn main() {
+    let num = 29;
+    println!("Is {} prime? {}", num, is_prime(num));
+}
